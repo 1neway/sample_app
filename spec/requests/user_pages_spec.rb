451aaa249e2ceebac07ce25a592ace0ac9c5441c
Abraham R. Sangha <abraham.sangha@gmail.com>
2013-03-28 10:12:39 -0400

    Admin

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -116,4 +116,20 @@ describe "User pages" do
 	  it { should have_selector('h1', text: user.name) }
 	  it { should have_selector('title', text: user.name) }
   end
+
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
 end
