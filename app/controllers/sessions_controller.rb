feb2e31116263ea74d2dcc17901e7e56da8eea79
Abraham R. Sangha <abraham.sangha@gmail.com>
2013-03-27 21:35:12 -0400

    Friendly Forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       # Sign the user in and redirect to the user's show page.
       # session[:user_id] = user.id
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
